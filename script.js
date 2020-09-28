1b32ece63284f03d86cf208572a913d818b218f3
Greg Stead <gregstead@gmail.com>
2020-09-27 22:11:26 -0500

    functional model complete

--- script.js
+++ script.js
@@ -12,32 +12,50 @@ function writePassword() {
 
 };
 
-function generatePassword() {\
+function generatePassword() {
 
   // Get the password length from user
   // Make sure input is a number between 8 and 128
-  var charChecked = false;
   var charLength = ''
-
-  while (charChecked != true){
+  var charChecked = false;
+  
+  while (charChecked != true) {
     charLength = parseInt(prompt("Number of characters? (Choose a number between 8 and 128"))
-    if ((isNaN(charLength)) || (charLength >= 8 && charLength <= 128)) {
-      carChecked = false;
+    if ((isNaN(charLength)) || !(charLength >= 8 && charLength <= 128)) {
+      charChecked = false;
     } else {
-      return charLength;
-      carChecked = true;
+      charLength = charLength;
+      charChecked = true;
     }
   }
+  
+  characterPool = ''
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = lowerCase.toUpperCase();
+  var allNumbers = "0123456789";
+  var allSpecialChars = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
 
   // Does password contain uppercase?
-  
+  var containsUpperCase = confirm("Password contains uppercase letters? \n\n OK - Yes \n Cancel - No");
+  if (containsUpperCase){
+    characterPool += upperCase;
+  };
   // Does password contain lowercase
-
+  var containsLowerCase = confirm("Password contains lowercase letters? \n\n OK - Yes \n Cancel - No");
+  if (containsLowerCase) {
+    characterPool += lowerCase;
+  };
   // Does password contain numbers?
-
+  var containsNumbers = confirm("Password contains numbers? \n\n OK - Yes \n Cancel - No");
+  if (containsNumbers) {
+    characterPool += allNumbers;
+  };
   // Does password special characters?
-
-
+  var containsSpecialChars = confirm("Password contains special characters? \n\n OK - Yes \n Cancel - No");
+  if (containsSpecialChars){
+    characterPool += allSpecialChars;
+  };
+  
   // Is at least one of the following true? contains-upper contains-lower contains-num contains-spec
 
   // for i in charLength
@@ -45,8 +63,14 @@ function generatePassword() {\
   //  Choose a random number between 0 and (x.length() - 1)
   //  return character
 
+  var password = '';
+  for (i=0; i < charLength; i++){
+    password += characterPool[Math.floor(Math.random() * characterPool.length)];
+  };
 
-  }
+  return password;
+
+
 };
 
 // Add event listener to generate button
