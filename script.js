3afa016ed3d321216d69c6b286b753ae3fd5f603
Greg Stead <gregstead@gmail.com>
2020-09-27 19:52:01 -0500

    Finished password length input and sketched out psuedo code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,7 +10,44 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
+
+function generatePassword() {\
+
+  // Get the password length from user
+  // Make sure input is a number between 8 and 128
+  var charChecked = false;
+  var charLength = ''
+
+  while (charChecked != true){
+    charLength = parseInt(prompt("Number of characters? (Choose a number between 8 and 128"))
+    if ((isNaN(charLength)) || (charLength >= 8 && charLength <= 128)) {
+      carChecked = false;
+    } else {
+      return charLength;
+      carChecked = true;
+    }
+  }
+
+  // Does password contain uppercase?
+  
+  // Does password contain lowercase
+
+  // Does password contain numbers?
+
+  // Does password special characters?
+
+
+  // Is at least one of the following true? contains-upper contains-lower contains-num contains-spec
+
+  // for i in charLength
+  //  Choose a random var x = [uppercase lowercase number specialchar ]
+  //  Choose a random number between 0 and (x.length() - 1)
+  //  return character
+
+
+  }
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
