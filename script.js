27d4204dc9c3093632d687554a56cbdc56c87eae
Greg Stead <gregstead@gmail.com>
2020-09-30 21:43:09 -0500

    Added while loop to check if at least one character list is selected

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ function generatePassword() {
   var charChecked = false;
   
   while (charChecked != true) {
-    charLength = parseInt(prompt("Number of characters? (Choose a number between 8 and 128"))
+    charLength = parseInt(prompt("Number of characters? (Choose a number between 8 and 128"));
+    console.log(charLength);
     if ((isNaN(charLength)) || !(charLength >= 8 && charLength <= 128)) {
       charChecked = false;
     } else {
@@ -37,34 +38,44 @@ function generatePassword() {
   //   "specialChar" : "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/",
   // };
 
+
+  var charSelected = false;
+
   characterPool = ''
   var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseList = lowercaseList.toUpperCase();
   var numbersList = "0123456789";
   var specialCharList = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
 
-  // Does password contain uppercaseList?
-  var uppercaseConfirm = confirm("Password contains uppercase letters? \n\n OK - Yes \n Cancel - No");
-  if (uppercaseConfirm){
-    characterPool += uppercaseList;
+  while (charSelected == false) {
+
+    // Does password contain uppercaseList?
+    var uppercaseConfirm = confirm("Password contains uppercase letters? \n\n OK - Yes \n Cancel - No");
+    if (uppercaseConfirm){
+      characterPool += uppercaseList;
+      charSelected = true;
+    };
+    // Does password contain lowercaseList
+    var lowercaseConfirm = confirm("Password contains lowercase letters? \n\n OK - Yes \n Cancel - No");
+    if (lowercaseConfirm) {
+      characterPool += lowercaseList;
+      charSelected = true;
+    };
+    // Does password contain numbers?
+    var numbersConfirm = confirm("Password contains numbers? \n\n OK - Yes \n Cancel - No");
+    if (numbersConfirm) {
+      characterPool += numbersList;
+      charSelected = true;
+    };
+    // Does password special characters?
+    var specialCharConfirm = confirm("Password contains special characters? \n\n OK - Yes \n Cancel - No");
+    if (specialCharConfirm){
+      characterPool += specialCharList;
+      charSelected = true;
+    };
   };
-  // Does password contain lowercaseList
-  var lowercaseConfirm = confirm("Password contains lowercase letters? \n\n OK - Yes \n Cancel - No");
-  if (lowercaseConfirm) {
-    characterPool += lowercaseList;
-  };
-  // Does password contain numbers?
-  var numbersConfirm = confirm("Password contains numbers? \n\n OK - Yes \n Cancel - No");
-  if (numbersConfirm) {
-    characterPool += numbersList;
-  };
-  // Does password special characters?
-  var specialCharConfirm = confirm("Password contains special characters? \n\n OK - Yes \n Cancel - No");
-  if (specialCharConfirm){
-    characterPool += specialCharList;
-  };
-  
   // Is at least one of the following true? contains-upper contains-lower contains-num contains-spec
+
 
   // for i in charLength
   //  Choose a random var x = [uppercaseList lowercaseList number specialchar ]
@@ -72,14 +83,21 @@ function generatePassword() {
   //  return character
 
   var password = '';
+  
+  // does password contain one of each character type requested?
+  
   for (i=0; i < charLength; i++){
     password += characterPool[Math.floor(Math.random() * characterPool.length)];
   };
 
   return password;
 
-
 };
+
+// Takes a String and returns a Boolean
+function passwordCheck(password) {
+  
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
